0b1c2d23d6ad2df736e57107859c9d989f1ead1a
Nicolas Tran <snicolastran@hotmail.fr>
2022-08-30 14:18:17 +0200

    1.2.2
    Creation of classes SavingsAccount and CurrentAccount

---
+++ NtBank/src/components/CurrentAccount.java
@@ -0,0 +1,11 @@
+package components;
+
+public class CurrentAccount extends Account{
+
+	public CurrentAccount(String label, Client client) {
+		super(label, client);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ NtBank/src/components/SavingsAccount.java
@@ -0,0 +1,10 @@
+package components;
+
+public class SavingsAccount extends Account{
+
+	public SavingsAccount(String label, Client client) {
+		super(label, client);
+		// TODO Auto-generated constructor stub
+	}
+
+}
