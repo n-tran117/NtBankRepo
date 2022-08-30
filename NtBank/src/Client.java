66c496aae8fd766da0fff528d1f969d4fd0cf118
Nicolas Tran <snicolastran@hotmail.fr>
2022-08-30 11:16:28 +0200

    Client constructor creation

--- NtBank/src/Client.java
+++ NtBank/src/Client.java
@@ -3,8 +3,12 @@ public class Client {
 	
 	private String name;
 	private String firstName;
-	private static int clientNumber;
+	private static int clientNumber=0;
 	
-	
+	public Client(String name, String firstName) {
+		this.name = name;
+		this.firstName = firstName;
+		clientNumber++;
+	}
 
 }
